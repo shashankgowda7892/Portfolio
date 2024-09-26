446419ccaf666cfe04e6fbfb8230623057dbed03
Shashank Gowda C <shashankgowda8548.com>
2024-09-26 20:57:11 +0530

    first commit

---
+++ src/Details.js
@@ -0,0 +1,105 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/code.svg"
+// Profile Image
+import profile from "./assets/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+
+import java from "./assets/techstack/java.svg";
+import eclipse from "./assets/techstack/eclipse.svg";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+
+import tailwind from "./assets/techstack/tailwind.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Shashank Gowda C",
+  tagline: "I build things for web",
+  img: profile,
+  about: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/",
+  github: "https://github.com/shashankgowda7892/",
+  twitter: "https://twitter.com/",
+
+};
+
+
+
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  tailwind: tailwind,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  java:java,
+  eclipse:eclipse,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Child-Identification-System",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript, Python",
+    previewLink: "https://github.com/shashankgowda7892/Child-Identification-System",
+    githubLink: "https://github.com/shashankgowda7892/Child-Identification-System",
+  },
+  {
+    title: "Food-Management-System",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript, PHP, MySQl",
+    previewLink: "https://github.com/shashankgowda7892/food-management-system",
+    githubLink: "https://github.com/shashankgowda7892/food-management-system",
+  },
+  {
+    title: "Accident-Detection-System",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "Python, ML ",
+    previewLink: "https://github.com/shashankgowda7892/Accident-Detection-System",
+    githubLink: "https://github.com/shashankgowda7892/Accident-Detection-System",
+  },
+  
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "shashankgowda8548@gmail.com",
+  phone: "+91 7892871357",
+};
